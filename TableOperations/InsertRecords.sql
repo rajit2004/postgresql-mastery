d27a16efbf17466de4ce39e9b114b9319e4e54db
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-17 19:07:00 +0530

    Table Insertion queries

---
+++ TableOperations/InsertRecords.sql
@@ -0,0 +1,11 @@
+-- first entry
+insert into students(id, name, gender, dob)
+values (1,'Ranesh' , 'M' , date '20/12/2004');
+
+-- second entry
+insert into students(id, name, gender, dob)
+values (2 , 'Aniket' , 'M' , date '26/04/2004');
+
+-- third entry
+insert into students(id, name, gender, dob)
+VALUES (3,'Arham' , 'M' , date '23/08/2004')
