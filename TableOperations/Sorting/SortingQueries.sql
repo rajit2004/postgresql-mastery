fe81ec9129f8ae6208c2317d60a8e951ab354d53
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-18 13:59:40 +0530

    Sorting queries

---
+++ TableOperations/Sorting/SortingQueries.sql
@@ -0,0 +1,8 @@
+select * from residents order by country desc ;
+select * from residents order by country;
+
+select * from students order by dob desc;
+
+select * from residents order by first_name;
+
+select * from residents  where email is not null order by email;
