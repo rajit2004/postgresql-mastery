ec80660ac597ca333ce5f073231fef1a64d27f11
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-18 14:00:42 +0530

    Solution to LeetCode 1148 with runtime = 375ms.

--- LeetCode/SQL50/Select/LeetCode_1148_ArticleViews_I.sql
+++ LeetCode/SQL50/Select/LeetCode_1148_ArticleViews_I.sql
@@ -19,3 +19,4 @@ Write a solution to find all the authors that viewed at least one of their own a
 Return the result table sorted by id in ascending order.
 */
 
+select distinct author_id as id from Views where author_id = viewer_id order by author_id;
