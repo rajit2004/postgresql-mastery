4c6c445dfcc73029b8ce7175596b2e27d0411619
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-18 22:12:44 +0530

    Solution to LeetCode 1141 with runtime = 408ms.

---
+++ LeetCode/SQL50/Sorting and Grouping/LeetCode_1141_UserActivityforthePast30DaysI.sql
@@ -0,0 +1,30 @@
+/*
+Table: Activity
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| session_id    | int     |
+| activity_date | date    |
+| activity_type | enum    |
++---------------+---------+
+This table may have duplicate rows.
+The activity_type column is an ENUM (category) of type ('open_session', 'end_session', 'scroll_down', 'send_message').
+The table shows the user activities for a social media website.
+Note that each session belongs to exactly one user.
+
+
+Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Note: Any activity from ('open_session', 'end_session', 'scroll_down', 'send_message') will be considered valid activity for a user to be considered active on a day.
+*/
+
+select activity_date as day , count(distinct user_id) as active_users
+from Activity
+where activity_date between '2019-06-28' AND '2019-07-27'
+group by day;
