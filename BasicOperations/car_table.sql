59f55b10d8fd957182657989dd64562abce4be26
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-19 16:06:43 +0530

    creation of sample table for queries

---
+++ BasicOperations/car_table.sql
@@ -0,0 +1,11 @@
+create table car (
+	car_make VARCHAR(50),
+	car_model VARCHAR(50),
+	car_year int,
+	car_color VARCHAR(50),
+	car_price DECIMAL(8,2),
+	car_mileage INT,
+	car_condition VARCHAR(19),
+	car_location VARCHAR(50),
+	car_owner VARCHAR(50)
+);
